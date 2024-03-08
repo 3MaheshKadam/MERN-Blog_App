2ab6028d47ba4ec044f59db52406c77b2eec02ee
3MaheshKadam <maheshkadam9298@gmail.com>
2024-03-08 17:21:39 +0530

    changes made in fils to create a test router

--- api/index.js
+++ api/index.js
@@ -1,7 +1,20 @@
+import { configDotenv } from 'dotenv';
 import express from 'express';
+import mongoose from 'mongoose';
+import userRouter from './routes/user.routes.js';
+configDotenv()
+
+mongoose 
+.connect(process.env.url)
+.then(console.log("db conn established"))
+.catch((err)=>{
+    console.log(err);
+});
 
 const app =express();
 
 app.listen(3000,()=>{
     console.log("server is running on port 3000!!!");
-});
+}); 
+
+app.use('/api/user',userRouter);

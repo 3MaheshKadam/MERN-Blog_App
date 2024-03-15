c3b248842d443fc80fc41f114d62bdb22f1c11ec
3MaheshKadam <maheshkadam9298@gmail.com>
2024-03-15 19:31:49 +0530

    dark mode n header updated with current user's dara

--- client/src/Components/Header.jsx
+++ client/src/Components/Header.jsx
@@ -1,49 +1,80 @@
-import React from 'react';
-import {Button, Navbar ,TextInput} from 'flowbite-react';
-import { Link ,useLocation } from 'react-router-dom';
-import {AiOutlineSearch} from 'react-icons/ai';
-import { FaMoon } from 'react-icons/fa';
+import { Avatar, Button, Dropdown, Navbar, TextInput } from 'flowbite-react';
+import { Link, useLocation } from 'react-router-dom';
+import { AiOutlineSearch } from 'react-icons/ai';
+import { FaMoon, FaSun } from 'react-icons/fa';
+import { useSelector, useDispatch } from 'react-redux';
+import { toggleTheme } from '../redux/theme/themeSlice';
 
-
-const Header = () => {
-
-  const path =useLocation().pathname;
-
+export default function Header() {
+  const path = useLocation().pathname;
+  const dispatch = useDispatch();
+  const { currentUser } = useSelector((state) => state.user);
+  const { theme } = useSelector((state) => state.theme);
   return (
     <Navbar className='border-b-2'>
     <Link to="/" className='self-center whitespace-nowrap text-sm sm:text-xl font-semibold dark:text-white'>
     <span className='px-2 py-1 bg-gradient-to-r from-red-500 via-orange-500 to-yellow-300 rounded-lg text-white'>Kadam's </span>Blog
       </Link>
       <form>
-        <TextInput type='text' placeholder='search...'
-        rightIcon={AiOutlineSearch}
-        className='hidden lg:inline'>
-        </TextInput>
+        <TextInput
+          type='text'
+          placeholder='Search...'
+          rightIcon={AiOutlineSearch}
+          className='hidden lg:inline'
+        />
       </form>
-      <Button className='w-12 h10 lg:hidden' color='gray' pill><AiOutlineSearch/></Button>
+      <Button className='w-12 h-10 lg:hidden' color='gray' pill>
+        <AiOutlineSearch />
+      </Button>
       <div className='flex gap-2 md:order-2'>
-        <Button className='w-12 h10  sm:inline'pill color='gray'>
-          <FaMoon/>
+        <Button
+          className='w-12 h-10 hidden sm:inline'
+          color='gray'
+          pill
+          onClick={() => dispatch(toggleTheme())}
+        >
+          {theme === 'light' ? <FaSun /> : <FaMoon />}
         </Button>
-        <Link to='/signin'>
-          <Button gradientDuoTone='redToOrange' outline >Sign IN</Button>
-        </Link>
-        <Navbar.Toggle/>
+        {currentUser ? (
+          <Dropdown
+            arrowIcon={false}
+            inline
+            label={
+              <Avatar alt='user' img={currentUser.profilePicture} rounded />
+            }
+          >
+            <Dropdown.Header>
+              <span className='block text-sm'>@{currentUser.username}</span>
+              <span className='block text-sm font-medium truncate'>
+                {currentUser.email}
+              </span>
+            </Dropdown.Header>
+            <Link to={'/dashboard?tab=profile'}>
+              <Dropdown.Item>Profile</Dropdown.Item>
+            </Link>
+            <Dropdown.Divider />
+            <Dropdown.Item>Sign out</Dropdown.Item>
+          </Dropdown>
+        ) : (
+          <Link to='/signin'>
+            <Button gradientDuoTone='pinkToOrange' outline>
+              Sign In
+            </Button>
+          </Link>
+        )}
+        <Navbar.Toggle />
       </div>
-        <Navbar.Collapse>
-          <Navbar.Link active={path ==="/"} as={"div"}>
-         
-            <Link to='/'>Home</Link>
-          </Navbar.Link>
-          <Navbar.Link active={path ==="/about"}as={"div"}> 
-            <Link to='/about'>About</Link>
-          </Navbar.Link>
-          <Navbar.Link active={path ==="/projects"}as={"div"}>
-            <Link to='/projects'>Projects</Link>
-          </Navbar.Link>
-        </Navbar.Collapse>
+      <Navbar.Collapse>
+        <Navbar.Link active={path === '/'} as={'div'}>
+          <Link to='/'>Home</Link>
+        </Navbar.Link>
+        <Navbar.Link active={path === '/about'} as={'div'}>
+          <Link to='/about'>About</Link>
+        </Navbar.Link>
+        <Navbar.Link active={path === '/projects'} as={'div'}>
+          <Link to='/projects'>Projects</Link>
+        </Navbar.Link>
+      </Navbar.Collapse>
     </Navbar>
-  )
+  );
 }
-
-export default Header

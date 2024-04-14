071f8575c508b7fe2536d82e12c4858a15d81f7b
3MaheshKadam <maheshkadam9298@gmail.com>
2024-04-14 20:44:13 +0530

    delete post functionality

--- client/src/Components/DashPosts.jsx
+++ client/src/Components/DashPosts.jsx
@@ -7,6 +7,7 @@ export default function DashPosts() {
   const { currentUser } = useSelector((state) => state.user);
   const [userPosts, setUserPosts] = useState([]);
   console.log(userPosts);
+  const [showMore , setShowMore] = useState(true);
   useEffect(() => {
     const fetchPosts = async () => {
       try {
@@ -14,6 +15,9 @@ export default function DashPosts() {
         const data = await res.json();
         if (res.ok) {
           setUserPosts(data.posts);
+        }
+        if(data.posts.length<9){
+          setShowMore(false);
         }
       } catch (error) {
         console.log(error.message);
@@ -24,6 +28,25 @@ export default function DashPosts() {
     }
   }, [currentUser._id]);
 
+  const handleShowMore=async()=>{
+    const startIndex = userPosts.length;
+    try{
+      const res = await fetch(
+        `/api/post/getposts?userId=${currentUser._id}&startindex=${startIndex}`
+      );
+      const data =  await res.json();
+      if(res.ok){
+          setUserPosts((prev)=>[...prev, ...data.posts]);
+          if(data.posts.length <9){
+            setShowMore(false);
+          }
+      }
+    }
+    catch(error){
+      console.log(error.message);
+    }
+
+  };
   return (
     <div className='table-auto overflow-x-scroll md:mx-auto p-3 scrollbar scrollbar-track-slate-100 scrollbar-thumb-slate-300 dark:scrollbar-track-slate-700 dark:scrollbar-thumb-slate-500'>
       {currentUser.isAdmin && userPosts.length > 0 ? (
@@ -80,6 +103,9 @@ export default function DashPosts() {
               </Table.Body>
             ))}
           </Table>
+          {showMore &&(
+            <button onClick={handleShowMore} className='w-full my-7 text-red-400 self-center text-sm '>Show-More</button>
+          )}
         </>
       ) : (
         <p>You have no posts yet!</p>

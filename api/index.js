b7ecd71b53f565641096d1dd637c8404433b49d2
3MaheshKadam <maheshkadam9298@gmail.com>
2024-06-11 20:20:42 +0530

    render

--- api/index.js
+++ api/index.js
@@ -4,39 +4,45 @@ import dotenv from 'dotenv';
 import userRoutes from './routes/user.route.js';
 import authRoutes from './routes/auth.route.js';
 import cookieParser from 'cookie-parser';
-import postRouters from './routes/post.route.js'
+import postRouters from './routes/post.route.js';
 import commentRoutes from './routes/comment.route.js';
 import path from 'path';
+
+// Load environment variables from .env file
 dotenv.config();
 
-mongoose
-  .connect(process.env.url)
+// Connect to MongoDB
+mongoose.connect(process.env.url, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => {
-    console.log(`MongoDb is connected `);
+    console.log('MongoDB is connected');
   })
   .catch((err) => {
-    console.log(err);
+    console.error('Error connecting to MongoDB', err);
   });
 
 const app = express();
+const PORT = process.env.PORT || 3000;
 
-const __dirname=path.resolve();
-
+// Middleware
 app.use(express.json());
 app.use(cookieParser());
-app.listen(3000, () => {
-  console.log('Server is running on port 3000!');
-});
 
+// API Routes
 app.use('/api/user', userRoutes);
 app.use('/api/auth', authRoutes);
-app.use('/api/post',postRouters);
+app.use('/api/post', postRouters);
 app.use('/api/comment', commentRoutes);
 
-app.use(express.static(path.join(__dirname,'/client/dist')));
-app.get('*',(req,res)=>{
-  res.sendFile(path.join(__dirname,"client" ,"dist" ,"index.html"));
-})
+// Serve static files from the React app
+const __dirname = path.resolve();
+app.use(express.static(path.join(__dirname, 'client', 'dist')));
+
+// The "catchall" handler: for any request that doesn't match one above, send back React's index.html file.
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
+});
+
+// Error handling middleware
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
   const message = err.message || 'Internal Server Error';
@@ -45,4 +51,9 @@ app.use((err, req, res, next) => {
     statusCode,
     message,
   });
+});
+
+// Start the server
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
